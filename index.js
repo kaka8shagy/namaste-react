59a4215e9ac4f58d92796c117d96c235c0b35512
Sagar Wakchaure <kaka8shagy@gmail.com>
2025-05-17 19:54:30 +0530

    create heading using react

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log('index.js working')
+const heading = React.createElement('h1', {}, 'Hello World from React!');
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(heading);
