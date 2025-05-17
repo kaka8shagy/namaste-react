e6537dbff786ee3b9be984afd586edcd6c34bfc0
Sagar Wakchaure <kaka8shagy@gmail.com>
2025-05-17 20:24:29 +0530

    try complex html in react

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-const heading = React.createElement('h1', {id: 'main-heading'}, 'Hello World from React!');
+
+const child = React.createElement('div', { id: 'child' }, [
+    React.createElement('h1', {id: 'heading-1'}, 'Heading 1 in React'),
+    React.createElement('h2', {id: 'heading-2'}, 'Heading 2 in React'),
+]);
+const parent = React.createElement('div', { id: 'parent' }, child);
+console.log(parent)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading);
+root.render(parent);
