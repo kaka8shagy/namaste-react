d4954888191652e3948044fea12234f008f57cd5
Sagar Wakchaure <kaka8shagy@gmail.com>
2025-05-19 19:46:16 +0530

    bug fix

--- index.js
+++ index.js
@@ -4,10 +4,8 @@ import ReactDOM from 'react-dom/client';
 
 // const heading = React.createElement('h1', {}, 'Namaste React')
 const heading = <h1 id="heading">Namaster React</h1>
-const data = () => console.log('abcd');
 const Heading = () => (
     <div>
-        {data}
         <h1 id="heading">Namaster React</h1>
     </div>
 );
